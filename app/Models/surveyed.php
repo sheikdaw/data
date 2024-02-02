91d1a2a7dd94434fd35dae7b6a8f9452a88f3cf3
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-02-02 09:45:46 +0530

    Update surveyed.php

--- app/Models/surveyed.php
+++ app/Models/surveyed.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\images;
+use App\Models\image;
 class surveyed extends Model
 {
     use HasFactory;
