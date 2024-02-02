a2a05deb22432dc67dbb3ff1521b7599ecf43e30
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-02-02 09:52:50 +0530

    Initial commit

--- database/migrations/2023_12_19_232710_create_images_table.php
+++ database/migrations/2023_12_19_232710_create_images_table.php
@@ -6,24 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('image');
-            $table->string('gisid');
+            $table->unsignedBigInteger('gisid'); // Adjust to the actual type of gisid
             $table->timestamps();
-            $table->foreign('gisid')->references('gisid')->on('surveyed');
 
+            // Add foreign key constraint
+            $table->foreign('gisid')->references('gisid')->on('surveyed');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('images');
