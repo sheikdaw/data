d5300c11729ba3b2260a5a58fe8ea4c97cd16712
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-02-02 09:36:03 +0530

    Update showqr.blade.php

--- resources/views/back/page/admin/showqr.blade.php
+++ resources/views/back/page/admin/showqr.blade.php
@@ -1,25 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="table-responsive card p-3">
-    <table class="table table-bordered">
-        <thead class="thead-dark"> <!-- Use thead-light for a light background -->
-            <tr>
-                <th>Ward</th>
-                <th>Assessment</th>
-                <th>Street Name</th>
-                <!-- Other table headers -->
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($surveys as $item)
+<head>
+    <title>Bootstrap 5 Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+
+    <div class="table-responsive card p-3">
+        <table class="table table-bordered">
+            <thead class="thead-dark"> <!-- Use thead-light for a light background -->
                 <tr>
-                    <td>{{ $item->ward }}</td>
-                    <td>{{ $item->assessment }}</td>
-                    <td>{{ $item->road_name }}</td>
-                    <!-- Display other columns accordingly -->
+                    <th>Ward</th>
+                    <th>Assessment</th>
+                    <th>Street Name</th>
+                    <th>Image</th>
+                    <!-- Other table headers -->
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                @foreach ($surveys as $item)
+                    <tr>
+                        <td>{{ $item->ward }}</td>
+                        <td>{{ $item->assessment }}</td>
+                        <td>{{ $item->road_name }}</td>
+                        <td>
 
+                                <img src="{{ public_path($item->image) }}" alt="" style="width: 150px; height: 150px;">
+                        </td>
+                        <!-- Display other columns accordingly -->
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
 
+</html>
