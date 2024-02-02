4018b4269711bbaf4e78cbbc7eb1ccd444173ac6
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-02-02 10:50:27 +0530

    Update 2023_12_19_232710_create_images_table.php

--- database/migrations/2023_12_19_232710_create_images_table.php
+++ database/migrations/2023_12_19_232710_create_images_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\surveyed;
 
 return new class extends Migration
 {
@@ -15,7 +16,10 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
+    public function surveyed()
+    {
+        return $this->belongsTo(Surveyed::class, 'gisid', 'gisid');
+    }
     public function down(): void
     {
         Schema::dropIfExists('images');
