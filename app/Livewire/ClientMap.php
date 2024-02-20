15fce2c7717592142b3bd5dcf9851b6f7bf1e34a
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-02-20 12:24:26 +0530

    Update ClientMap.php

--- app/Livewire/ClientMap.php
+++ app/Livewire/ClientMap.php
@@ -6,8 +6,16 @@ use Livewire\Component;
 
 class ClientMap extends Component
 {
+    public $surveyed;
+
+    public function mount()
+    {
+        // Fetch surveyed data and assign it to the property
+        $this->surveyed = Surveyed::all();
+    }
+
     public function render()
     {
-        return view('livewire.client-map');
+        return view('livewire.client-map', ['surveyed' => $this->surveyed]);
     }
 }
