77e1ba32bad4b28cfda6b226b1acb0e5bbd4200a
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-02-20 12:11:11 +0530

    Update MapComponent.php

--- app/Livewire/MapComponent.php
+++ app/Livewire/MapComponent.php
@@ -1,23 +1,13 @@
 <?php
 
-namespace App\Http\Livewire; // Adjust the namespace to match the correct directory structure
+namespace App\Livewire;
 
-use App\Models\Surveyed; // Import the Surveyed model
 use Livewire\Component;
 
 class MapComponent extends Component
 {
-    public $surveyed;
-
-    public function mount()
-    {
-        // Fetch surveyed data and assign it to the property
-        $this->surveyed = Surveyed::all();
-    }
-
     public function render()
     {
-        // Pass the $surveyed data to the view
-        return view('livewire.map-component', ['surveyed' => $this->surveyed]);
+        return view('livewire.map-component');
     }
 }
