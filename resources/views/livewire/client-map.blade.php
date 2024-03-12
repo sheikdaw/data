371e6989b39634cd93403f8f2b3d384ddcdbfa34
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-03-12 11:48:54 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -80,7 +80,7 @@
     </button>
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModalCenter filter" tabindex="-1" role="dialog"
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
         aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
