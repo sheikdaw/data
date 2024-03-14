298483a5d23924b57116be00a916fc1b73f0a70c
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-03-14 10:19:25 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -235,15 +235,15 @@
                     });
                     var overlays;
 
-                    var minX = 80.0; // Example minimum X coordinate
-                    var minY = 13.0; // Example minimum Y coordinate
-                    var maxX = 81.0; // Example maximum X coordinate
-                    var maxY = 14.0; // Example maximum Y coordinate
+                    // var minX = 80.0; // Example minimum X coordinate
+                    // var minY = 13.0; // Example minimum Y coordinate
+                    // var maxX = 81.0; // Example maximum X coordinate
+                    // var maxY = 14.0; // Example maximum Y coordinate
 
-//                     var minX =  -0.5; // Example minimum X coordinate
-// var minY = -25485.5; // Example minimum Y coordinate
-// var maxX =19040.5; // Example maximum X coordinate
-// var maxY =  0.5; // Example maximum Y coordinate
+                    var minX =  -0.5; // Example minimum X coordinate
+var minY = -25485.5; // Example minimum Y coordinate
+var maxX =19040.5; // Example maximum X coordinate
+var maxY =  0.5; // Example maximum Y coordinate
 
 
                     var imageExtent = [minX, minY, maxX, maxY];
