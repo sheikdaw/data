6bcd798e08b8fe2efa9e93811905889f0ff30ba1
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-03-14 14:05:28 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -255,7 +255,7 @@
                                 title: 'Converted Image',
                                 source: new ol.source.ImageStatic({
                                     url: pngFilePath, // URL of the converted image
-                                    projection: 'EPSG:3857',
+                                    projection: 'EPSG:4326',
                                     imageExtent: imageExtent
                                 })
                             })
