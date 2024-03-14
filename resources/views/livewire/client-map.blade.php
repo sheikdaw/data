f9547ff0e9a965da626869291c1f77246d79d91e
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-03-14 16:21:24 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -234,10 +234,10 @@
                         source: vectorBuildingSource
                     });
                     var overlays;
-                    var minX = 8566150.76848; // Example minimum X coordinate
-                    var minY = 1232901.87763; // Example minimum Y coordinate
-                    var maxX = 8568107.06848; // Example maximum X coordinate
-                    var maxY = 1235527.17763; // Example maximum Y coordinate
+                    var minX = 80.0; // Example minimum X coordinate
+                    var minY = 13.0; // Example minimum Y coordinate
+                    var maxX = 81.0; // Example maximum X coordinate
+                    var maxY = 14.0; // Example maximum Y coordinate
 
                     var imageExtent = [minX, minY, maxX, maxY];
 
@@ -248,8 +248,9 @@
                                 title: 'Converted Image',
                                 source: new ol.source.ImageStatic({
                                     url: pngFilePath,
-                                    projection: 'EPSG:3857',
+                                    projection: 'EPSG:4326',
                                     imageExtent: imageExtent,
+                                    imageSize: [5328, 4048] // Provide the size of your image
                                 })
                             })
                         ]
