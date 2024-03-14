1c8d7ef8a24bb1bbe9c47f596909d7db814ae22b
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-03-14 12:27:26 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -255,7 +255,7 @@ var maxY =  0.5; // Example maximum Y coordinate
                                 title: 'Converted Image',
                                 source: new ol.source.ImageStatic({
                                     url: pngFilePath, // URL of the converted image
-                                    projection: 'EPSG:4326',
+                                    projection: 'EPSG:3857',
                                     imageExtent: imageExtent
                                 })
                             })
