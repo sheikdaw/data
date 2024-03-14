b1027a2b45d24aedfb24c60816a15d70c34f7341
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-03-14 16:06:22 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -196,7 +196,7 @@
 
             var pointpath = "{{ $point }}";
             var buildingpath = "{{ asset('public/kovai/building.json') }}";
-            var pngFilePath = "{{ asset('public/kovai/new/png2.img') }}";
+            var pngFilePath = "{{ asset('public/kovai/new/png2.png') }}";
 
             var pointJsonPromise = fetch(pointpath)
                 .then(response => {
