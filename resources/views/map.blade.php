69a1c8d6a7e0507a6f6b7b883e927da0d6b2084b
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-03-16 13:43:23 +0530

    Update map.blade.php

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -14,58 +14,53 @@
 <body>
     <div id="map"></div>
     <script>
-        // Sample XML data
-var xmlData = `
-<image>
-    <boundingBox>
-        <minX>-180</minX>
-        <minY>-90</minY>
-        <maxX>180</maxX>
-        <maxY>90</maxY>
-    </boundingBox>
-</image>
-`;
-var pngFilePath = "{{ asset('public/kovai/new/png1.png.aux.xml.png') }}";
-// Parse the XML data
-var parser = new DOMParser();
-var xmlDoc = parser.parseFromString(xmlData, "text/xml");
+        var pngFilePath = "{{ asset('public/kovai/new/png1.png') }}";
+        var xmlFilePath = "{{ asset('public/kovai/new/png1.png.aux.xml') }}";
 
-// Extract bounding box coordinates
-var minX = parseFloat(xmlDoc.getElementsByTagName("minX")[0].textContent);
-var minY = parseFloat(xmlDoc.getElementsByTagName("minY")[0].textContent);
-var maxX = parseFloat(xmlDoc.getElementsByTagName("maxX")[0].textContent);
-var maxY = parseFloat(xmlDoc.getElementsByTagName("maxY")[0].textContent);
+        // Load the XML file
+        fetch(xmlFilePath)
+            .then(response => response.text())
+            .then(xmlData => {
+                var parser = new DOMParser();
+                var xmlDoc = parser.parseFromString(xmlData, "text/xml");
 
-// Log the coordinates
-console.log("MinX:", minX);
-console.log("MinY:", minY);
-console.log("MaxX:", maxX);
-console.log("MaxY:", maxY);
+                // Extract bounding box coordinates
+                var minX = parseFloat(xmlDoc.getElementsByTagName("minx")[0].textContent);
+                var minY = parseFloat(xmlDoc.getElementsByTagName("miny")[0].textContent);
+                var maxX = parseFloat(xmlDoc.getElementsByTagName("maxx")[0].textContent);
+                var maxY = parseFloat(xmlDoc.getElementsByTagName("maxy")[0].textContent);
 
-        // var extent = [-20037508.34, -20037508.34, 20037508.34, 20037508.34]; // WGS 1984 Web Mercator Auxiliary Sphere extent
-        // var projection = 'EPSG:4326'; // WGS 1984 Web Mercator Auxiliary Sphere projection
+                console.log("MinX:", minX);
+                console.log("MinY:", minY);
+                console.log("MaxX:", maxX);
+                console.log("MaxY:", maxY);
 
-        // var imageLayer = new ol.layer.Image({
-        //     source: new ol.source.ImageStatic({
-        //         url: pngFilePath, // Update with the path to your PNG file
-        //         projection: projection,
-        //         imageExtent: extent // Set the extent of your image
-        //     })
-        // });
+                var extent = [minX, minY, maxX, maxY];
+                var projection = 'EPSG:4326';
 
-        // var map = new ol.Map({
-        //     layers: [
-        //         new ol.layer.Tile({
-        //             source: new ol.source.OSM()
-        //         }),
-        //         imageLayer
-        //     ],
-        //     target: 'map',
-        //     view: new ol.View({
-        //         center: [0, 0],
-        //         zoom: 2
-        //     })
-        // });
+                var imageLayer = new ol.layer.Image({
+                    source: new ol.source.ImageStatic({
+                        url: pngFilePath,
+                        projection: projection,
+                        imageExtent: extent
+                    })
+                });
+
+                var map = new ol.Map({
+                    layers: [
+                        new ol.layer.Tile({
+                            source: new ol.source.OSM()
+                        }),
+                        imageLayer
+                    ],
+                    target: 'map',
+                    view: new ol.View({
+                        center: ol.extent.getCenter(extent),
+                        zoom: 2
+                    })
+                });
+            })
+            .catch(error => console.error('Error fetching XML:', error));
     </script>
 </body>
 </html>
