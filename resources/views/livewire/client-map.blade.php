1b8da9906851909491afc2d1b0873c9151b27317
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-03-21 12:10:23 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -276,6 +276,9 @@
         stroke: new ol.style.Stroke({
             color: 'red',
             width: 2
+        }),
+        fill: new ol.style.Fill({
+            color: 'rgba(255, 0, 0, 0.1)' // Red fill with opacity
         })
     });
 };
