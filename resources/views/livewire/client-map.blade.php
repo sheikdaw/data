eb8482376be946755bff2ae90b99257424a8c7f4
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-03-30 10:00:35 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -255,8 +255,7 @@
                         view: new ol.View({
                             center: ol.proj.fromLonLat([76.955393, 11.020899]),
                             projection: 'EPSG:3857',
-                            zoom: 20,
-                            minZoom: 10
+                            zoom: 20
                         })
                     });
                     // Function to create style with text label and red border
