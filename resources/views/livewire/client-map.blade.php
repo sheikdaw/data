f81909490a08c0b0a7c2666e1ba51b8d2ce62d01
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-04-01 16:30:49 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -537,10 +537,10 @@
                             //         feature.setStyle(clickedStyle);
                             //     }
                             // });
-                            vectorBuildingLayer.setStyle(function(feature) {
-                                var id = feature.get('OBJECTID'); // Extract Id from feature properties
-                                return createLabelStyleFunction(id);
-                            });
+                            // vectorBuildingLayer.setStyle(function(feature) {
+                            //     var id = feature.get('OBJECTID'); // Extract Id from feature properties
+                            //     return createLabelStyleFunction(id);
+                            // });
                         })
                         .catch(error => {
                             console.error('Error refreshing map and data:', error);
