738e5dc963b6803e55b58d4958f9b42497f55246
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-04-02 17:40:44 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -122,7 +122,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <img src="{{asset('images/1.jpg')}}" alt="" width="100px">
+                    <img src="{{asset('public/images/1.jpg')}}" alt="" width="100px">
                     <h4>Feature Properties</h4>
                     <ul id="featurePropertiesList">
                         <!-- Feature properties will be displayed here -->
