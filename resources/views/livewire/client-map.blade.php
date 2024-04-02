f9d6928b8af2a57b99d6b1e267670938b9996459
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-04-02 09:54:41 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -56,6 +56,18 @@
             }
         </style>
     @endpush
+    @if(session('status'))
+    <div class="position-fixed top-0 end-0 p-3" style="z-index: 11">
+        <div class="toast align-items-center text-white bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body">
+                    {{ session('status') }}
+                </div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+@endif
 
     <form class="form-inline">
         <label for="type">Geometry type: &nbsp;</label>
@@ -526,6 +538,7 @@
                         if (value == 'Point') {
                             $.ajax({
                                 url: '/delete-feature', // URL to send the AJAX request
+                                type:post,
                                 data: value,
                                 success: function(response) {
                                     console.log(response.message);
@@ -538,6 +551,7 @@
                         } else if (value == "Polygon") {
                             $.ajax({
                                 url: '/delete-feature', // URL to send the AJAX request
+                                type:post,
                                 data: value,
                                 success: function(response) {
                                     console.log(response.message);
