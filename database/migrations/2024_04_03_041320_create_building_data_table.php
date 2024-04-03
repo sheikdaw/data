039ff5478b4f3bf6c2880b1072f785c5091f609c
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-04-03 16:51:40 +0530

    main

--- database/migrations/2024_04_03_041320_create_building_data_table.php
+++ database/migrations/2024_04_03_041320_create_building_data_table.php
@@ -11,29 +11,29 @@ return new class extends Migration
      */
     public function up(): void
     {
-         Schema::create('building_data', function (Blueprint $table) {
-        $table->id();
-        $table->string('gisid')->unique(); // Assuming gisid should be unique
-        $table->string('number_bill');
-        $table->string('number_floor');
-        $table->string('watet_tax'); // Corrected column name to 'water_tax'
-        $table->string('eb');
-        $table->string('building_name');
-        $table->string('building_usage');
-        $table->string('construction_type');
-        $table->string('road_name');
-        $table->string('ugd');
-        $table->string('rainwater_harvesting');
-        $table->string('parking');
-        $table->string('ramp');
-        $table->string('hoarding');
-        $table->string('cell_tower');
-        $table->string('solar_panel');
-        $table->string('water_connection');
-        $table->string('phone');
-        $table->string('image')->nullable();
-        $table->timestamps();
-    });
+        Schema::create('building_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('gisid');
+            $table->string('number_bill');
+            $table->string('number_floor');
+            $table->string('watet_tax');
+            $table->string('eb');
+            $table->string('building_name');
+            $table->string('building_usage');
+            $table->string('construction_type');
+            $table->string('road_name');
+            $table->string('ugd');
+            $table->string('rainwater_harvesting');
+            $table->string('parking');
+            $table->string('ramp');
+            $table->string('hoarding');
+            $table->string('cell_tower');
+            $table->string('solar_panel');
+            $table->string('water_connection');
+            $table->string('phone');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
