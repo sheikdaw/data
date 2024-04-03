f1f2612dd803cd613b232e461626d4657c202124
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-04-03 12:45:55 +0530

    Update FormController.php

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\surveyed;
 use App\Models\BuildingData;
+use App\Models\PointData;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
