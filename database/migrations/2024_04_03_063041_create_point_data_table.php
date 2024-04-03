bde70543822a98a3f2343775a7ca715155da3618
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-04-03 16:21:18 +0530

    Update 2024_04_03_063041_create_point_data_table.php

--- database/migrations/2024_04_03_063041_create_point_data_table.php
+++ database/migrations/2024_04_03_063041_create_point_data_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('assessment')->nullable();
             $table->string('old_assessment')->nullable();
-            $table->string('Floor')->nullable();
+            $table->string('floor')->nullable(); // Corrected column name to 'floor'
             $table->string('bill_usage')->nullable();
             $table->string('aadhar_no')->nullable();
             $table->string('ration_no')->nullable();
-            $table->string('phone_numnber')->nullable();
+            $table->string('phone_number')->nullable(); // Corrected column name to 'phone_number'
             $table->string('shop_floor')->nullable();
             $table->string('shop_name')->nullable();
             $table->string('shop_owner_name')->nullable();
@@ -28,7 +28,7 @@ return new class extends Migration
             $table->string('license')->nullable();
             $table->string('professional_tax')->nullable();
             $table->string('gst')->nullable();
-            $table->string('number_of_emplyee')->nullable();
+            $table->string('number_of_employee')->nullable(); // Corrected column name to 'number_of_employee'
             $table->string('trade_income')->nullable();
             $table->string('establishment_remarks')->nullable();
             $table->foreignId('gisid')->constrained('building_data')->onDelete('cascade');
