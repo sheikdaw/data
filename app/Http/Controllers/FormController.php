d36c7c4deb9758983363234ce6092eda48cc8331
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-04-05 10:29:41 +0530

    Update FormController.php

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -389,7 +389,7 @@ class FormController extends Controller
             'solar_panel' => 'required',
             'water_connection' => 'required',
             'phone' => 'required',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Example validation for image upload
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif', // Example validation for image upload
         ]);
 
         $buildingData = BuildingData::where('gisid', $validatedData['gisid'])->first();
