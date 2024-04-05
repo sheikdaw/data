b354072bb868d0b2ed4628fa3dc8fcefa98c6cd9
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-04-05 14:48:36 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -349,8 +349,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="gis">Gis</label>
-                                <input type="text" class="form-control" id="pointgis" name="point_gisid"
-                                    readonly>
+                                <input type="text" name="point_gisid" id="point_gisid" class="form-control" readonly />
                                 <div id="pointgis_error"></div>
                             </div>
                             <div class="form-group">
