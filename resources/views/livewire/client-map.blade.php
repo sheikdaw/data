c3dfe70ba1a26689592791002ff44ab535e1993d
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2024-04-08 14:40:28 +0530

    Update client-map.blade.php

--- resources/views/livewire/client-map.blade.php
+++ resources/views/livewire/client-map.blade.php
@@ -536,7 +536,7 @@
                         }
                         return response.json();
                     });
-                var lineJsonPromise = fetch(buildingpath)
+                var lineJsonPromise = fetch(linepath)
                     .then(response => {
                         if (!response.ok) {
                             throw new Error('Failed to load GeoJSON file');
